76e015e499b8798b5341329eb06a04f490770403
Ben Vien <ben.s.vien@gmail.com>
2025-01-09 09:13:15 +1100

    Update PointCloud_Merge.m

--- UAV Scripts/PointCloud_Merge.m
+++ UAV Scripts/PointCloud_Merge.m
@@ -1,15 +1,21 @@
-% 
+%%Matlab code to merge las files 
+%By Dr. Ben Vien
+%Version 1.0
+%Update: 9/01/2025
+
+
+%% 02: Define the output LAS file path
+%%
+outputFile = 'merged_filedummy.las'; 
+
+%% 01: las filename
 %Define the input LAS file paths
-%25W October 2024
+%
 lasFile1 = 'cloud2469e92110b5f28b_Block_0.las';  % Path to the first LAS file
 lasFile2 = 'cloud2469e92110b5f28b_Block_1.las';  % Path to the second LAS file
-% datetime('now')
-% Define the output LAS file path
-%%
-% Read the LAS files
-outputFile = 'merged_filedummy.las'; % Path for the merged LAS file
+% datetime('now') %Timecheck
 
-% Read the LAS files
+%% Read the LAS files
 tic
 reader1 = lasFileReader(lasFile1);
 reader2 = lasFileReader(lasFile2);
